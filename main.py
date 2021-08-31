bb24363e876a7a02d6f7f2f0e96aad69369087f0
Thomas Butler <10285945+thomaseb191@users.noreply.github.com>
2021-08-31 17:28:24 -0400

    Print success/fail

--- main.py
+++ main.py
@@ -25,12 +25,19 @@ accesstoken = input("Enter Private Access Token: ")
 session = requests.Session()
 session.headers.update({'PRIVATE-TOKEN': accesstoken})
 
+success = 0
+fail = 0
+
 for acc in accountsfile:
     email = acc.strip()
     query = {'email': email, 'access_level':accesslevel}
     response = session.post(gitlaburl + '/api/v4/groups/' + gitlabgroup + '/invitations', data=query)
     if (response.status_code == 201) and (response.json()['status'] == "success"):
         print(f'{email}: \033[92m SUCCESS: Invite Sent \033[0m')
+        success += 1
     else:
         print(f'{email}: \033[91m FAIL \033[0m')
-        print(response.json()['message'])    
+        print(response.json()['message'])
+        fail += 1
+
+print(f'{success} successful, {fail} failed')    
